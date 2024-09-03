823c762dc671d9eaa6f005f5a0c797a45f6f494a
MaksimZadar <maxzadarnovskiyy@gmail.com>
2024-09-03 20:04:59 +0930

    switch producer to web api

--- producer/Program.cs
+++ producer/Program.cs
@@ -1,7 +1,62 @@
-using producer;
+using Confluent.Kafka;
+using KafkaFlow;
+using KafkaFlow.Producers;
+using KafkaFlow.Serializer;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+var builder = WebApplication.CreateBuilder(args);
 
-var host = builder.Build();
-host.Run();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var brokers = builder.Configuration.GetSection("Kafka:BootstrapServers").Get<string[]>();
+var topic = builder.Configuration.GetSection("Kafka:Topic").Get<string>();
+const string ProducerName = "demo-monthly-invoice";
+
+builder.Services.AddKafka(k => k
+    .UseConsoleLog()
+    .AddCluster(c => c
+        .WithBrokers(brokers)
+        .CreateTopicIfNotExists(
+            topicName: topic,
+            numberOfPartitions: 3,
+            replicationFactor: 3)
+        .AddProducer(ProducerName, p => p
+            .DefaultTopic(topic)
+            .WithCompression(CompressionType.Gzip)
+            .AddMiddlewares(m => m
+                .AddSerializer<NewtonsoftJsonSerializer>()
+            )
+        )
+    )
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/demo", async (IProducerAccessor producer) =>
+{
+    await producer[ProducerName].ProduceAsync("demo", new DemoMessage
+    {
+        PolicyNumber = "NCI0001Q",
+        InvoiceYearMonth = DateTime.Now.AddMonths(-1)
+    });
+})
+.WithName("DemoMonthlyInvoice")
+.WithOpenApi();
+
+app.Run();
+
+class DemoMessage
+{
+    public string PolicyNumber { get; set; } = string.Empty;
+    public DateTime InvoiceYearMonth { get; set; }
+}
